2381864c90f2e38eda9d54ca495c72625b74ca3f
MD YASIR ARAFAT <yasirarafat.net@gmail.com>
2023-01-16 01:18:23 +0600

    Modified Author Controller for Minor Tweaks & AppSettings

--- BlazorBookStore.Api/Controllers/AuthorsController.cs
+++ BlazorBookStore.Api/Controllers/AuthorsController.cs
@@ -24,7 +24,7 @@ namespace BlazorBookStore.Api.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
         {
-            return await _context.Authors.ToListAsync();
+            return Ok(await _context.Authors.ToListAsync());
         }
 
         // GET: api/Authors/5
@@ -38,7 +38,7 @@ namespace BlazorBookStore.Api.Controllers
                 return NotFound();
             }
 
-            return author;
+            return Ok(author);
         }
 
         // PUT: api/Authors/5
@@ -59,7 +59,7 @@ namespace BlazorBookStore.Api.Controllers
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!AuthorExists(id))
+                if (!await AuthorExists(id))
                 {
                     return NotFound();
                 }
@@ -77,10 +77,10 @@ namespace BlazorBookStore.Api.Controllers
         [HttpPost]
         public async Task<ActionResult<Author>> PostAuthor(Author author)
         {
-            _context.Authors.Add(author);
+            await _context.Authors.AddAsync(author);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetAuthor", new { id = author.Id }, author);
+            return CreatedAtAction(nameof(GetAuthor), new { id = author.Id }, author);
         }
 
         // DELETE: api/Authors/5
@@ -99,9 +99,9 @@ namespace BlazorBookStore.Api.Controllers
             return NoContent();
         }
 
-        private bool AuthorExists(int id)
+        private async Task<bool> AuthorExists(int id)
         {
-            return _context.Authors.Any(e => e.Id == id);
+             return await _context.Authors.AnyAsync(e => e.Id == id);
         }
     }
 }

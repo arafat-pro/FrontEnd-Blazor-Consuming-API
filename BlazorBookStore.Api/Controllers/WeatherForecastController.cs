ca87377485e0c5170709000317d2e754a53cf445
MD YASIR ARAFAT <yasirarafat.net@gmail.com>
2023-01-15 22:06:18 +0600

    Added Seq Nuget Package & Modified AppSettings to send Log to Seq

--- BlazorBookStore.Api/Controllers/WeatherForecastController.cs
+++ BlazorBookStore.Api/Controllers/WeatherForecastController.cs
@@ -24,7 +24,7 @@ namespace BlazorBookStore.Api.Controllers
             _logger.LogInformation("Made call to Weather Endpoint");
             try
             {
-                throw new Exception("THis is our logging test exception");
+                throw new Exception("This is our logging test exception");
 
                 return Enumerable.Range(1, 5).Select(index => new WeatherForecast
                 {

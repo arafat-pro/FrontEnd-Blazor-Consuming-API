72b20f875badae4f472cbcc091932b0c9a671c52
MD YASIR ARAFAT <yasirarafat.net@gmail.com>
2023-01-16 00:51:56 +0600

    Added Connection String Configurations to Program File & DbContext Scaffolding from Existing Database

---
+++ BlazorBookStore.Api/Data/Author.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorBookStore.Api.Data;
+
+public partial class Author
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string? Bio { get; set; }
+
+    public virtual ICollection<Book> Books { get; } = new List<Book>();
+}
---
+++ BlazorBookStore.Api/Data/BookStoreDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorBookStore.Api.Data;
+
+public partial class BookStoreDbContext : DbContext
+{
+    public BookStoreDbContext()
+    {
+    }
+
+    public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.Property(e => e.FirstName).HasMaxLength(60);
+            entity.Property(e => e.LastName).HasMaxLength(60);
+            entity.Property(e => e.Bio).HasMaxLength(250);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasIndex(e => e.Isbn).IsUnique();
+            entity.Property(e => e.Image).HasMaxLength(250);
+            entity.Property(e => e.Isbn).HasMaxLength(20).HasColumnName("ISBN");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Summary).HasMaxLength(250);
+            entity.Property(e => e.Title).HasMaxLength(50);
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books).HasForeignKey(d => d.AuthorId);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}

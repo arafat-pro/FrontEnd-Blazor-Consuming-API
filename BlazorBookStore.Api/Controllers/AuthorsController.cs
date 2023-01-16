2a4a27460055b44340fe9d29e5ee9ef3e36a772b
MD YASIR ARAFAT <yasirarafat.net@gmail.com>
2023-01-17 02:58:29 +0600

    Added Static Folder & Message and Added Logging for Controller Actions.

--- BlazorBookStore.Api/Controllers/AuthorsController.cs
+++ BlazorBookStore.Api/Controllers/AuthorsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BlazorBookStore.Api.Data;
 using BlazorBookStore.Api.Models.Author;
+using BlazorBookStore.Api.Static;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,36 +13,57 @@ namespace BlazorBookStore.Api.Controllers
     {
         private readonly BookStoreDbContext _context;
         private readonly IMapper mapper;
+        private readonly ILogger<AuthorsController> logger;
 
-        public AuthorsController(BookStoreDbContext context, IMapper mapper)
+        public AuthorsController(BookStoreDbContext context, IMapper mapper, ILogger<AuthorsController> logger)
         {
             _context = context;
             this.mapper = mapper;
+            this.logger = logger;
         }
 
         // GET: api/Authors
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AuthorReadOnlyDto>>> GetAuthors()
         {
-            var authors = await _context.Authors.ToListAsync();
-            var authorDto = mapper.Map<IEnumerable<AuthorReadOnlyDto>>(authors);
-            return Ok(authorDto);
+            try
+            {
+                var authors = await _context.Authors.ToListAsync();
+                var authorDto = mapper.Map<IEnumerable<AuthorReadOnlyDto>>(authors);
+                return Ok(authorDto);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Error Performing Operation in {nameof(GetAuthors)}!");
+                return StatusCode(500, Messages.Error500Message);
+            }
+
         }
 
         // GET: api/Authors/5
         [HttpGet("{id}")]
         public async Task<ActionResult<AuthorReadOnlyDto>> GetAuthor(int id)
         {
-            var author = await _context.Authors.FindAsync(id);
-
-            if (author == null)
+            try
             {
-                return NotFound();
+                var author = await _context.Authors.FindAsync(id);
+
+                if (author == null)
+                {
+                    logger.LogWarning($"Record not found {nameof(GetAuthor)} - ID: {id}!");
+                    return NotFound();
+                }
+
+                var authorDto = mapper.Map<AuthorReadOnlyDto>(author);
+
+                return Ok(authorDto);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Error Performing Operation in {nameof(GetAuthor)}!");
+                return StatusCode(500, Messages.Error500Message); ;
             }
 
-            var authorDto = mapper.Map<AuthorReadOnlyDto>(author);
-
-            return Ok(authorDto);
         }
 
         // PUT: api/Authors/5
@@ -51,24 +73,25 @@ namespace BlazorBookStore.Api.Controllers
         {
             if (id != authorDto.Id)
             {
+                logger.LogWarning($"Update ID is Invalid in {nameof(PutAuthor)} - ID: {id}!");
                 return BadRequest();
             }
             var author = await _context.Authors.FindAsync(id);
 
             if (author == null)
             {
+                logger.LogWarning($"{nameof(Author)} record not found in {nameof(PutAuthor)} - ID: {id}!");
                 return NotFound();
             }
 
             mapper.Map(authorDto, author);
-
             _context.Entry(author).State = EntityState.Modified;
 
             try
             {
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
+            catch (DbUpdateConcurrencyException ex)
             {
                 if (!await AuthorExists(id))
                 {
@@ -76,10 +99,10 @@ namespace BlazorBookStore.Api.Controllers
                 }
                 else
                 {
-                    throw;
+                    logger.LogError(ex, $"Error Performing Operation in {nameof(PutAuthor)}!");
+                    return StatusCode(500, Messages.Error500Message); ;
                 }
             }
-
             return NoContent();
         }
 
@@ -88,27 +111,45 @@ namespace BlazorBookStore.Api.Controllers
         [HttpPost]
         public async Task<ActionResult<AuthorCreateDto>> PostAuthor(AuthorCreateDto authorDto)
         {
-            var author = mapper.Map<Author>(authorDto);
-            await _context.Authors.AddAsync(author);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var author = mapper.Map<Author>(authorDto);
+                await _context.Authors.AddAsync(author);
+                await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetAuthor), new { id = author.Id }, author);
+                return CreatedAtAction(nameof(GetAuthor), new { id = author.Id }, author);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Error Performing Operation in {nameof(PostAuthor)}!", authorDto);
+                return StatusCode(500, Messages.Error500Message); ;
+            }
+
         }
 
         // DELETE: api/Authors/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteAuthor(int id)
         {
-            var author = await _context.Authors.FindAsync(id);
-            if (author == null)
+            try
             {
-                return NotFound();
+                var author = await _context.Authors.FindAsync(id);
+                if (author == null)
+                {
+                    logger.LogWarning($"{nameof(Author)} record not found in {nameof(DeleteAuthor)} - ID: {id}!");
+                    return NotFound();
+                }
+
+                _context.Authors.Remove(author);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
             }
-
-            _context.Authors.Remove(author);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Error Performing Operation in {nameof(DeleteAuthor)}!");
+                return StatusCode(500, Messages.Error500Message); ;
+            }
         }
 
         private async Task<bool> AuthorExists(int id)
---
+++ BlazorBookStore.Api/Static/Messages.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorBookStore.Api.Static
+{
+    public static class Messages
+    {
+        public const string Error500Message = "Error Encountered! Please Try Again Later.";
+    }
+}
